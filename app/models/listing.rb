47a9ab5e6225adb657ceaefdc38d73e25c10ab44
Connor <10349@coderacademy.edu.au>
2022-06-27 16:24:15 +1000

    add condition enum in listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    poor: 1,
+    used: 2,
+    good_condition: 3,
+    brand_new: 4
+  }
 end
