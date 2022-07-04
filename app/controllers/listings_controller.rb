a69cd309801ee7cac0ed9b5eb13fbc1e8e23f48e
Connor <10349@coderacademy.edu.au>
2022-07-04 10:18:49 +1000

    implement authetnicate  using a before_action in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :set_form_vars
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   # GET /listings or /listings.json
   def index
